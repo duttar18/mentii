2a0ea4df4792a86415fd4abe0fbc724e8df5e7eb
Raul Dutta <duttaraul@gmail.com>
2020-07-26 03:53:12 -0400

    home

--- mentii/src/Home.js
+++ mentii/src/Home.js
@@ -3,65 +3,55 @@ import { Grid, Typography } from "@material-ui/core";
 import Mentor from "./Mentor";
 import Footer from "./Footer";
 import PrimarySearchAppBar from "./Appbar";
-const Home = () => {
-    return (
-        <div>
+
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            streams: []
+        };
+    }
+    componentDidMount() {
+        fetch("/api/streams", {
+            credentials: "same-origin",
+            method: "GET",
+        })
+        .then((response) => {
+            if (!response.ok) throw Error(response.statusText);
+            return response.json();
+        })
+        .then((data) => {
+            this.setState(data);
+        });
+    }
+    render() {
+        return (
+            <div>
             <PrimarySearchAppBar></PrimarySearchAppBar>
             <Grid item container>
-                <Grid xs={0} sm={2} />
                 <Grid item xs={12} sm={8}>
                     <Typography variant="h6" style={{ marginTop: "50px" }}>
-                        Computer Programming Advisors
+                        Advisors
                     </Typography>
                     <Grid container spacing={4}>
-                        <Grid item xs={12} sm={4}>
-                            <Mentor
-                                title="Daniel Zheng"
-                                imgSrc="https://i.postimg.cc/Qxt1VSSb/DSC09829-ARW.jpg"
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Mentor
-                                title="Raul Dutta"
-                                imgSrc="https://i.postimg.cc/kMjtrK9t/raul.jpg"
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Mentor
-                                title="James Li"
-                                imgSrc="https://i.postimg.cc/SN32R4nS/james.jpg"
-                            />
-                        </Grid>
-                    </Grid>
-                    <Typography variant="h6" style={{ marginTop: "100px" }}>
-                        General Advice
-                    </Typography>
-                    <Grid container spacing={4}>
-                        <Grid item xs={12} sm={4}>
-                            <Mentor
-                                title="Daniel Zheng"
-                                imgSrc="https://i.postimg.cc/Qxt1VSSb/DSC09829-ARW.jpg"
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Mentor
-                                title="Raul Dutta"
-                                imgSrc="https://i.postimg.cc/kMjtrK9t/raul.jpg"
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Mentor
-                                title="James Li"
-                                imgSrc="https://i.postimg.cc/SN32R4nS/james.jpg"
-                            />
-                        </Grid>
+                        {this.state.streams.map((streamer) => (
+                            <Grid item xs={12} sm={4}>
+                                <Mentor
+                                    name={streamer.name}
+                                    username={"/Watch/"+streamer.username}
+                                    imgSrc={streamer.avatar}
+
+                                />
+                            </Grid>
+                        ))}
                     </Grid>
                 </Grid>
-                <Grid item xs={0} sm={2} />
             </Grid>
             <Footer />
         </div>
-    );
-};
+        );
+    }
+}
 
 export default Home;
+
